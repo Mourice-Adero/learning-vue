5c2d511712635e9b1453ad4cc8f406dab4acb53a
Mourice-Adero <aderomourice7@gmail.com>
2025-06-26 12:34:50 +0300

    Done: Reactivity Fundamentals

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,15 @@
 import ConditionalRendering from "./components/ConditionalRendering.vue";
 import EventHandling from "./components/EventHandling.vue";
 import FormInputBinding from "./components/FormInputBinding.vue";
+import ReactivityFundamentals from "./components/ReactivityFundamentals.vue";
 </script>
 <template>
   <h1>Learning Vue</h1>
   <div>
+    <h3>Reactivity Fundamentals</h3>
+    <ReactivityFundamentals />
+    <br />
+    <br />
     <h3>Form Input Binding</h3>
     <FormInputBinding />
     <br />
---
+++ src/components/ReactivityFundamentals.vue
@@ -0,0 +1,41 @@
+<script setup>
+//Declaring reactive state
+import { ref } from "vue";
+
+// const users = reactive({}) --- Only works with state for object types
+const count = ref(0);
+console.log(count); // {value: 0}
+console.log(count.value); // 0
+</script>
+
+<template>
+  <p>Increment</p>
+  <button @click="count++">add 1</button>
+  <p>Count = {{ count }}</p>
+</template>
+
+<!-- Manually Exposing state Without using <script setup> -->
+<!-- <script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const count = ref(0);
+
+    function increment() {
+      count.value++;
+    }
+
+    return {
+      count,
+      increment,
+    };
+  },
+};
+</script>
+
+<template>
+  <p>Increment</p>
+  <button @click="count++">add 1</button>
+  <p>Count = {{ count }}</p>
+</template> -->
